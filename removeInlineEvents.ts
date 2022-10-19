05ecf97784746d1b3c10699c022eaebf715cc396
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-10-19 13:50:24 +0800

    fix: loop error in removeInlineEvents

--- removeInlineEvents.ts
+++ removeInlineEvents.ts
@@ -2,7 +2,7 @@ const onEvent = /^on.+/i;
 
 export function removeInlineEvents(frag:DocumentFragment) {
     frag.querySelectorAll("*").forEach(elem => {
-        for (const {name} of elem.attributes) {
+        for (const {name} of Object.values(elem.attributes)) {
             if (onEvent.test(name))
                 elem.removeAttribute(name);
         }
