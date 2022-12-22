5e48809a1126e1a785c71aecbe30718342a0d657
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-12-22 21:54:07 +0800

    Feat: simplify extracting uuid

--- lib/extractUUID.ts
+++ lib/extractUUID.ts
@@ -3,9 +3,7 @@
  */
 export function extractUUID(txt?:string):string {
     if (typeof txt == "string") {
-        const parts = txt.toLocaleLowerCase().split(":")
-        if (parts.includes("uuid"))
-            return parts[parts.length - 1];
+        return txt.replace("uuid:","");
     }
 
     return ""
