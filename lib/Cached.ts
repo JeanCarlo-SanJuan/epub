c13616c6e8ad260c64989a787b6f5eb0a99b3fa9
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-12-23 11:06:33 +0800

    Feat: rename EpubMS, prepare_memo as well as add comments

--- lib/Cached.ts
+++ lib/Cached.ts
@@ -3,33 +3,39 @@ import { cacheOrMiss } from "./cacheOrMiss";
 import { Epub, epub, EpubArgs } from ".";
 import { SanitizedEpub } from "./Sanitized";
 
-export const MemoizedEpub = prepare_memo(epub);
-export const EpubMS = prepare_memo(SanitizedEpub);
-export function prepare_memo<A extends EpubArgs,EP extends Epub>(implementation:(a:A)=>Promise<EP>) {
-    return async(a:A) => {
+export const MemoizedEpub = prepareMemo(epub);
+export const MemoizedEpubAndSanitized = prepareMemo(SanitizedEpub);
+
+/**
+ * Replaces the methods in import("./index.ts").DataReader with cacheable ones.
+ */
+export function asMemoized<EP extends Epub>(base:EP) {
+    return {
+        ...base,
+        cache:new BookCache(),
+        async getContent(id: string) {
+            return cacheOrMiss(
+                this.cache.text,
+                base.getContent.bind(base),
+                id
+            )
+        },
+        async getImage(id: string) {
+            return cacheOrMiss(
+                this.cache.image,
+                base.getImage.bind(base),
+                id
+            )
+        }
+    }
+}
+
+/**
+ * Composes a function that returns an `Epub` with {@link asMemoized}
+ */
+export function prepareMemo<HasEpubArgs extends EpubArgs,EpubLike extends Epub>(implementation:(a:HasEpubArgs)=>Promise<EpubLike>) {
+    return async(a:HasEpubArgs) => {
         const book = await implementation(a);
-        const cache = new BookCache()
-        async function getContent(id: string) {
-            return cacheOrMiss(
-                cache.text,
-                book.getContent.bind(book),
-                id
-            )
-        }
-    
-        async function getImage(id: string) {
-            return cacheOrMiss(
-                cache.image,
-                book.getImage.bind(book),
-                id
-            )
-        }
-    
-        return {
-            ...book,
-            cache,
-            getContent,
-            getImage
-        }
+        return asMemoized(book);
     }
 }
