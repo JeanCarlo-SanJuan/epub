23a75bebbb4faa8dc81798ad81b730feebbb0958
Jean Carlo San Juan <sanjuan.jeancarlo@gmail.com>
2022-12-23 10:20:55 +0800

    Feat(matchSource): Refactor nested code

--- lib/matchSource.ts
+++ lib/matchSource.ts
@@ -5,25 +5,23 @@ export async function matchMediaSources<D extends DataReader>(d:D, m:Manifest, f
     for (const img of Array.from(frag.querySelectorAll<HTMLImageElement | SVGImageElement>("img, image"))) {
         //TODO: Allow a default image to be used when no src.
         const { key, src } = getSource(img);
+        if (src === undefined || key === undefined) {
+            throw Error(`${img} HAS NO SOURCE`)
+        }
+        
+        img.dataset.src = src;
 
-        if (src && key) {
-            img.dataset.src = src;
-
-            const item = Object.values(m).find(item =>
-                src?.endsWith(item.href)
-            )
-
-            if (item) {
-                img.setAttribute(key,
-                    await d.getImage(item.id)
-                );
-                continue;
-            }
-
+        const item = Object.values(m).find(item =>
+            src?.endsWith(item.href)
+        )
+        
+        if (item === undefined) {
             throw Error(`${src} IS NOT IN MANIFEST`)
         }
 
-        throw Error(`${img} HAS NO SOURCE`)
+        img.setAttribute(key,
+            await d.getImage(item.id)
+        );
     }
 }
 
